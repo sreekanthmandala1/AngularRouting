5dfe3216b7cfa9107689b6a69dfb9e7fa87baeff
sreekanth <sreekanthmandalaa@gmail.com>
2021-07-20 15:48:35 +0530

    services basics

--- src/app/components/page-thirteen/page-thirteen.component.ts
+++ src/app/components/page-thirteen/page-thirteen.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { UsersService } from 'src/app/services/users.service';
 
 @Component({
   selector: 'app-page-thirteen',
   templateUrl: './page-thirteen.component.html',
-  styleUrls: ['./page-thirteen.component.css']
+  styleUrls: ['./page-thirteen.component.css'],
 })
 export class PageThirteenComponent implements OnInit {
-
-  constructor() { }
+  users:any;
+  constructor(private usersService : UsersService) { }
 
   ngOnInit(): void {
+    this.usersService.getUsers().subscribe(data=>{
+      this.users=data;
+    })
+    
+    // this.users = this.usersService.getUsers();
+    // const users = this.usersService.getUsers();
+    // console.log(users)
   }
 
 }
---
+++ src/app/services/users.service.ts
@@ -0,0 +1,25 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+  constructor(private http:HttpClient) { }
+
+  getUsers(){
+   return this.http.get('https://jsonplaceholder.typicode.com/users')
+    // const users = [
+    //   {
+    //     name:'sreekanth',
+    //     gender : 'male'
+    //   },
+    //   {
+    //     name:'kiran',
+    //     gender : 'male'
+    //   }
+    // ];
+    // return users;
+  }
+}
